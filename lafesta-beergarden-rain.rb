81c29251d004b877c1a28afacc9372da866231d9
gecko655 <aqwsedrft1234@yahoo.co.jp>
2016-07-06 01:14:42 +0900

    Update

--- lafesta-beergarden-rain.rb
+++ lafesta-beergarden-rain.rb
@@ -3,4 +3,10 @@ require "slack-ruby-client"
 require "open-uri"
 
 doc = Nokogiri::HTML(open("http://lafesta-beergarden.com/"))
-p doc.css(".stats_2").first.content
+is_open_string = doc.css(".stats_2").first.content
+Slack.configure do |config|
+  config.token = ENV["TOKEN"]
+end
+client = Slack::Web::Client.new
+
+client.chat_postMessage(channel: '#general', text: 'La festa beer garden is ' + is_open_string + ' today.', as_user: false)
