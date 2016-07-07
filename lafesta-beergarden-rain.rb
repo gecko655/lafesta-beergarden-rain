2db1afe0d7206eb9121cb3aad6ee8560af1fb533
yamamori.akihiro <yamamori.akihiro@donuts.ne.jp>
2016-07-07 13:28:45 +0900

    Implemented notification switch

--- lafesta-beergarden-rain.rb
+++ lafesta-beergarden-rain.rb
@@ -2,11 +2,15 @@ require "nokogiri"
 require "slack-ruby-client"
 require "open-uri"
 
+force_notify=(ARGV[0]=="yes")
+
 doc = Nokogiri::HTML(open("http://lafesta-beergarden.com/"))
 is_open_string = doc.css(".stats_2").first.content
+
+
 Slack.configure do |config|
   config.token = ENV["TOKEN"]
 end
 client = Slack::Web::Client.new
 
-client.chat_postMessage(channel: '#general', text: 'La festa beer garden is ' + is_open_string + ' today.', as_user: false)
+client.chat_postMessage(channel: '#general', text: '本日、La festa BEER GARDEN は"' + is_open_string + '"しています', as_user: false) if (force_notify || ! is_open_string.casecmp("open").zero?)
