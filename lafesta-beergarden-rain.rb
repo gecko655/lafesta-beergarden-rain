091f9e6bc73b281344413bd060f12083b9c59b7f
yamamori.akihiro <yamamori.akihiro@donuts.ne.jp>
2016-07-07 14:22:20 +0900

    Update

--- lafesta-beergarden-rain.rb
+++ lafesta-beergarden-rain.rb
@@ -11,6 +11,7 @@ is_open_string = doc.css(".stats_2").first.content
 Slack.configure do |config|
   config.token = ENV["TOKEN"]
 end
+channel_name = ENV["CHANNEL"]
 client = Slack::Web::Client.new
 
-client.chat_postMessage(channel: '#general', text: '本日、La festa BEER GARDEN は"' + is_open_string + '"しています', as_user: false) if (force_notify || ! is_open_string.casecmp("open").zero?)
+client.chat_postMessage(channel: channel_name, text: '本日、La festa BEER GARDEN は"' + is_open_string + '"しています', as_user: false) if (force_notify || ! is_open_string.casecmp("open").zero?)
